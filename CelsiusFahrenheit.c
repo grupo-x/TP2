cb2893154193889d3d49b02a376f3157bc6dc3c1
Grupo k2051 <danycuomo96@gmail.com>
2017-04-17 09:11:27 -0300

    Update CelsiusFahrenheit.c

--- CelsiusFahrenheit.c
+++ CelsiusFahrenheit.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "Conversiones.h"
 
 /*Programa que convierte temperaturas de Celsius a Fahrenheit y viceversa. De 0 grados a 300 grados con intervalos de 20 grados.*/
-
-float celsiusAFahrenheit(int temperatura)
-{
-	return (9.0/5.0) * temperatura + 32;
-}
-
-float fahrenheitACelsius(int temperatura)
-{
-	return (5.0/9.0) * (temperatura - 32);
-}
 
 int main(void) 
 {
@@ -19,18 +9,14 @@ int main(void)
 	
 	printf("Celsius\t| Fahrenheit\n");
 	
-	int i = 0;
-	
-	for (i = lower; i <= upper; i = i + 20)
+	for (int i = lower; i <= upper; i = i + 20)
 	{
 		printf ("%d\t| %4.1f\n", i, celsiusAFahrenheit(i));
 	}
 	
 	printf("\n\nFahrenheit\t| Celsius\n");
 	
-	i = 0;
-	
-	for (i = lower; i <= upper; i = i + 20)
+	for (int i = lower; i <= upper; i = i + 20)
 	{
 		printf ("%d\t\t| %4.1f\n", i, fahrenheitACelsius(i));
 	}
