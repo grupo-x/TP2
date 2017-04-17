f68047fcaba35b971f1600681d0e557fe89938f7
Grupo k2051 <danycuomo96@gmail.com>
2017-04-17 09:37:38 -0300

    Add files via upload

---
+++ Conversiones.c
@@ -0,0 +1,11 @@
+#include "Conversiones.h"
+
+double fahrenheitACelsius(double f)
+{	
+ 	return (5.0/9.0)*(f-32);
+}
+
+double celsiusAFahrenheit(double c)
+{
+	return (9.0/5.0) * f + 32
+}
---
+++ Conversiones.h
@@ -0,0 +1,7 @@
+#ifndef CONVERSION_H_INCLUDED
+#define CONVERSION_H_INCLUDED
+
+double celsiusAFahrenheit(double);
+double fahrenheitACelsius(double);
+
+#endif
